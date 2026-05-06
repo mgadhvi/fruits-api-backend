cf6a32b4e2a53dd3b9d197f86c1bf08bcca57824
Manish Gadhvi <Manish.Gadhvi@lafosse.com>
2026-05-06 12:39:08 +0100

    Added functionality for cors in express application

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //How to run the program
 const express = require('express')
+const cors = require('cors')
 const app = express();
 const fruits = require("./routes/fruits")
 
@@ -7,6 +8,7 @@ app.get('/', (req, res) => {
     res.send("Hello from the fruits api")
 })
 
+app.use(cors())
 app.use(express.json())
 app.use('/fruits', fruits) // http://localhost:3000/fruits
 
